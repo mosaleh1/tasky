b2fdd709120310aaa2d654504c6e9a4694e9b1f1
saleh <mohamedsalehofficial@gmail.com>
2025-09-20 03:05:16 +0300

    Initialize project with dependencies and basic configuration

---
+++ src/test/java/ar/meetus/tasky/TaskyApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.meetus.tasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
